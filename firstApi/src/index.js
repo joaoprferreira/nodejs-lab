1744886517a29c808c4dd926e13b0a73e580da09
João Pedro Ferreira <joaof082@gmail.com>
2025-02-01 16:39:16 -0300

    initial commit

---
+++ firstApi/src/index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const { URL } = require('url')
+
+const UserController = require('./controllers/UserController')
+
+const routes = require('./routes')
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+  console.log(parsedUrl)
+
+  let { pathname } = parsedUrl
+  let id = null
+
+  const splitEndpoint = pathname.split('/').filter(Boolean)
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`
+    id = splitEndpoint[1]
+  }
+
+  console.log('splitedEnpoint:::', splitEndpoint)
+
+  console.log(`Request method: ${request.method} | Endpoint: ${pathname}`)
+
+  const route = routes.find(
+    (routesObject) =>
+      routesObject.endpoint === pathname &&
+      routesObject.method === request.method
+  )
+
+  console.log('ROUTE::', !!route)
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
+    request.params = { id }
+
+    route.handler(request, response)
+  } else {
+    response.writeHead(404, { 'content-type': 'text/html' })
+    response.end(`Cannot ${request.method} ${pathname}`)
+  }
+})
+
+server.listen(3000, () =>
+  console.log('Server started at http://localhost:3000')
+)
---
+++ firstApi/src/mocks/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'João Pedro',
+    description: 'descrição ',
+  },
+  {
+    id: 2,
+    name: 'João Pedro',
+    description: 'descrição ',
+  },
+  {
+    id: 3,
+    name: 'João Pedro',
+    description: 'descrição ',
+  },
+]
